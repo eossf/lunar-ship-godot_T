882d571807328560a5205b15926fc0fb4df2e6f2
metairie <stephane.metairie@gmail.com>
2023-09-01 20:44:54 +0200

    lunar commands ok

--- Lunar.gd
+++ Lunar.gd
@@ -1,31 +1,41 @@
 extends RigidBody2D
 
-const THRUST_Y = 100000
-const THRUST_X = 50000
-var torque = 10000
-var rotation_direction = 0
+const TORQUE = 200000
+const ROTATION_STEP = 20
+const THRUST_STEP = 5000
+const THRUST_MAX = 50000
+
+var thrust = 0
 var current_rotation = 0
-var angle = deg_to_rad(0)
-const ROTATION_STEP = 5
+var latest_rotation = 0
+var angle = 0
 
 func _integrate_forces(state):
 	if Input.is_action_pressed("jump"):
-		angle = deg_to_rad(current_rotation)
-		state.apply_force(Vector2(0 , -THRUST_Y))
-		print("transform.basis " + str(transform))
+		thrust += THRUST_STEP
+		thrust = clamp(thrust, 0, THRUST_MAX)
+		state.apply_force(Vector2(0 , -thrust))
 	else:
+		thrust = 0
+		current_rotation = 0
+		angle = 0
 		state.apply_force(Vector2.ZERO)
-		rotation_direction = 0
 	
 	if Input.is_action_pressed("move_right"):
-#		rotation_direction += ROTATION_STEP
-#		current_rotation = current_rotation + rotation_direction
-		state.apply_force(Vector2(THRUST_X , 0))
+		if sign(latest_rotation) != sign(current_rotation):
+			current_rotation = 0
+		current_rotation += ROTATION_STEP
+		latest_rotation = current_rotation
 	if Input.is_action_pressed("move_left"):
-#		rotation_direction -= ROTATION_STEP
-#		current_rotation = current_rotation - rotation_direction
-		state.apply_force(Vector2(-THRUST_X , 0))
-	
+		if sign(latest_rotation) != sign(current_rotation):
+			current_rotation = 0
+		current_rotation -= ROTATION_STEP
+		latest_rotation = current_rotation
 	# rotation
-	if rotation_direction != 0:
-		state.apply_torque(rotation_direction * torque)
+	if current_rotation != 0:
+		angle = deg_to_rad(current_rotation)
+		state.apply_torque(angle * TORQUE)
+	else:
+		state.apply_torque(angle * TORQUE)
+		state.apply_force(Vector2(thrust/5 * global_rotation , 0))
+		
