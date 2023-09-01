db8a2e22cc2900b503b55be4e6a0b43df06f39b6
metairie <stephane.metairie@gmail.com>
2023-09-01 18:21:32 +0200

    no rotation - apply forces down left right only

--- Lunar.gd
+++ Lunar.gd
@@ -1,16 +1,31 @@
 extends RigidBody2D
 
-var thrust = Vector2(0, -200000)
-var torque = 500000
+const THRUST_Y = 100000
+const THRUST_X = 50000
+var torque = 10000
+var rotation_direction = 0
+var current_rotation = 0
+var angle = deg_to_rad(0)
+const ROTATION_STEP = 5
 
 func _integrate_forces(state):
 	if Input.is_action_pressed("jump"):
-		state.apply_force(thrust)
+		angle = deg_to_rad(current_rotation)
+		state.apply_force(Vector2(0 , -THRUST_Y))
+		print("transform.basis " + str(transform))
 	else:
-		state.apply_force(Vector2())
-	var rotation_direction = 0
+		state.apply_force(Vector2.ZERO)
+		rotation_direction = 0
+	
 	if Input.is_action_pressed("move_right"):
-		rotation_direction += 1
+#		rotation_direction += ROTATION_STEP
+#		current_rotation = current_rotation + rotation_direction
+		state.apply_force(Vector2(THRUST_X , 0))
 	if Input.is_action_pressed("move_left"):
-		rotation_direction -= 1
-	state.apply_torque(rotation_direction * torque)
+#		rotation_direction -= ROTATION_STEP
+#		current_rotation = current_rotation - rotation_direction
+		state.apply_force(Vector2(-THRUST_X , 0))
+	
+	# rotation
+	if rotation_direction != 0:
+		state.apply_torque(rotation_direction * torque)
